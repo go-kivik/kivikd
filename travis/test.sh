ee02e6ac933fe14b2b6124eabcb604f879f77456
Jonathan Hall <flimzy@flimzy.com>
2017-10-23 22:58:27 +0200

    Fix coverage tests

--- travis/test.sh
+++ travis/test.sh
@@ -18,11 +18,11 @@ case "$1" in
     "coverage")
         echo "" > coverage.txt
 
-        TEST_PKGS=$(find -name "*_test.go" | grep -v /vendor/ | xargs dirname | sort -u | sed -e "s#^\.#github.com/go-kivik/couchdb#" )
+        TEST_PKGS=$(find -name "*_test.go" | grep -v /vendor/ | xargs dirname | sort -u | sed -e "s#^\.#github.com/go-kivik/kivikd#" )
 
         for d in $TEST_PKGS; do
             go test -i $d
-            DEPS=$((go list -f $'{{range $f := .TestImports}}{{$f}}\n{{end}}{{range $f := .Imports}}{{$f}}\n{{end}}' $d && echo $d) | sort -u | grep -v /vendor/ | grep -v /pouchdb | grep -v /kivik/test | grep ^github.com/flimzy/kivik | tr '\n' ' ')
+            DEPS=$((go list -f $'{{range $f := .TestImports}}{{$f}}\n{{end}}{{range $f := .Imports}}{{$f}}\n{{end}}' $d && echo $d) | sort -u | grep -v /vendor/ | grep ^github.com/go-kivik/kivikd | tr '\n' ' ')
             go test -coverprofile=profile.out -covermode=set -coverpkg=$(join_list $DEPS) $d
             if [ -f profile.out ]; then
                 cat profile.out >> coverage.txt
